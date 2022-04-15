7d88e5d749257770d9c2fc5f4640c517619b18b4
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2022-04-15 14:25:20 +0530

    Add password controller

--- src/models/db.py
+++ src/models/db.py
@@ -23,3 +23,16 @@ class Bookmark(db.Model):
         self.bookmark_title = bookmark_title
         self.bookmark_url = bookmark_url
         self.bookmark_desc = bookmark_desc
+
+class Passowrd(db.Model):
+    password_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    password_title = db.Column(db.String(165), nullable=False)
+    password_desc = db.Column(db.String(1024), nullable=True)
+    password = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, user_id, password_title, password_desc, password):
+        self.user_id = user_id
+        self.password_title = password_title
+        self.password_url = password_url
+        self.password = password
