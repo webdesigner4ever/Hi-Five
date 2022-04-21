af79ef5445f2f33c9380c2856593020121daf406
snehasachu <snehajayaprakash2017@gmail.com>
2022-04-21 15:19:01 +0530

    Notes Encryption

--- src/utility.py
+++ src/utility.py
@@ -1,4 +1,8 @@
 import re
+import pyaes, pbkdf2, binascii, os, secrets
+
+key = "abcdefghijklmnws"
+iv=b"abcdeophijklmnws"
 
 def password_check(password):
     length_error = len(password) < 8
@@ -10,3 +14,52 @@ def password_check(password):
     password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
 
     return password_ok
+
+def encryption(s):
+    global key
+    global iv  
+    aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+    ciphertext = aes.encrypt(s)
+    return binascii.hexlify(ciphertext)
+
+def decryption(s):
+    global key
+    global iv  
+    aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+    decrypted = aes.decrypt(s)
+    return decrypted
+
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+
+    def __init__(self): 
+        global key
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+c = AESCipher()
+x = c.encrypt("hello")
+assert "hello" == c.decrypt(x)
