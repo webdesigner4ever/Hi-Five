33354026156d23a50bb907855c3c071f6353c63c
Yadhu Krishna M <yadhukrishna.mpm@gmail.com>
2022-04-21 20:16:57 +0530

    Update note encryption

--- src/utility.py
+++ src/utility.py
@@ -1,42 +1,30 @@
 import re
-import pyaes, pbkdf2, binascii, os, secrets
-
-key = "abcdefghijklmnws"
-iv=b"abcdeophijklmnws"
+import base64
+import hashlib
+from config import key, iv
+from Crypto import Random
+from Crypto.Cipher import AES
 
 def password_check(password):
     length_error = len(password) < 8
     digit_error = re.search(r"\d", password) is None
     uppercase_error = re.search(r"[A-Z]", password) is None
     lowercase_error = re.search(r"[a-z]", password) is None
-    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]', password) is None
 
-    password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
+    password_ok = not (
+        length_error
+        or digit_error
+        or uppercase_error
+        or lowercase_error
+        or symbol_error
+    )
 
     return password_ok
 
-def encryption(s):
-    global key
-    global iv  
-    aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
-    ciphertext = aes.encrypt(s)
-    return binascii.hexlify(ciphertext)
-
-def decryption(s):
-    global key
-    global iv  
-    aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
-    decrypted = aes.decrypt(s)
-    return decrypted
-
-import base64
-import hashlib
-from Crypto import Random
-from Crypto.Cipher import AES
 
 class AESCipher(object):
-
-    def __init__(self): 
+    def __init__(self):
         global key
         self.bs = AES.block_size
         self.key = hashlib.sha256(key.encode()).digest()
@@ -49,17 +37,13 @@ class AESCipher(object):
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
+        iv = enc[: AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        return self._unpad(cipher.decrypt(enc[AES.block_size :])).decode("utf-8")
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
 
     @staticmethod
     def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
-
-c = AESCipher()
-x = c.encrypt("hello")
-assert "hello" == c.decrypt(x)
+        return s[: -ord(s[len(s) - 1 :])]
